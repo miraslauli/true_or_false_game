b0a22d93ad095187046f136de37eb5c6037941af
miraslauli <mirchyhir@gmail.com>
2023-03-13 22:13:40 +0100

    Version 1.4

--- src/enums.py
+++ src/enums.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class GameStatus(Enum):     # Status of our game
+class GameStatus(Enum):
     WON = 1
     LOST = 2
     IN_PROGRESS = 3
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ print(game.question_generation())
 print(game.set_the_number_of_attempts(int(input("Set the number of attempts: "))))
 print(game.check_game_status)
 
-while game.game_status == GameStatus.IN_PROGRESS:   # game started
+while game.game_status == GameStatus.IN_PROGRESS:
 
     print(game.question())
     print(game.give_answer(input("Enter your answer: ")))
