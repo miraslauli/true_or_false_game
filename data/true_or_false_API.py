6161d751dec8996d0a8df5a32af6f83d4824b27d
miraslauli <mirchyhir@gmail.com>
2023-03-13 02:55:30 +0100

    Version 1.1

--- data/true_or_false_API.py
+++ data/true_or_false_API.py
@@ -5,25 +5,25 @@ from custom_errors import InvalidOperationException
 
 class TrueOrFalse:
     def __init__(self):
-        self.questions: dict[int:list[str]] = {}
-        self.game_file = Path('Questions.csv')
+        self.questions: dict[int : list[str]] = {}
+        self.game_file = Path("Questions.csv")
         self.number_of_attempts: int = 3
         self.current_question: int = 0
         self.game_status = GameStatus.NOT_STARTED
 
     def question_generation(self) -> str:
         key: int = 0
-        questions: dict[int:list[str]] = {}
+        questions: dict[int : list[str]] = {}
 
-        with open(self.game_file, 'r') as game_file:
+        with open(self.game_file, "r") as game_file:
             for line in game_file:
-                questions[key] = line.rstrip().split(';')
+                questions[key] = line.rstrip().split(";")
                 key += 1
 
         self.questions = questions
         self.game_status = GameStatus.IN_PROGRESS
 
-        return f'The questions have been generated'
+        return f"The questions have been generated"
 
     def question(self) -> str:
         user_question = self.questions[self.current_question]
@@ -31,11 +31,11 @@ class TrueOrFalse:
 
     def path_to_file(self, file: str) -> str:
         self.game_file = Path(file)
-        return f'Path to file: {self.game_file}'
+        return f"Path to file: {self.game_file}"
 
     def set_the_number_of_attempts(self, number: int) -> str:
         self.number_of_attempts = number
-        return f'The number of attempts: {self.number_of_attempts}'
+        return f"The number of attempts: {self.number_of_attempts}"
 
     def next_question(self) -> str:
         self.current_question += 1
@@ -44,25 +44,27 @@ class TrueOrFalse:
             self.questions[self.current_question]
         except KeyError:
             self.game_status = GameStatus.WON
-            return 'You win!'
+            return "You win!"
 
-        return f'Current question: {self.current_question + 1}'
+        return f"Current question: {self.current_question + 1}"
 
     def give_answer(self, answer: str) -> str:
 
         if self.game_status != GameStatus.IN_PROGRESS:
-            raise InvalidOperationException(f'Inappropriate status of game: {self.game_status}')
+            raise InvalidOperationException(
+                f"Inappropriate status of game: {self.game_status}"
+            )
 
         user_question = self.questions[self.current_question]
         if answer == user_question[1]:
-            return f'This is the correct answer. {user_question[2]}'
+            return f"This is the correct answer. {user_question[2]}"
         else:
             self.number_of_attempts -= 1
 
             if self.number_of_attempts == 0:
                 self.game_status = GameStatus.LOST
                 print(f"That's the wrong answer. {user_question[2]}")
-                return 'You lost!'
+                return "You lost!"
 
             return f"That's the wrong answer. {user_question[2]}"
 
