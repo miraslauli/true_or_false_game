ee853276b19c8341a5eef86fc35da1cb0c89a9de
miraslauli <mirchyhir@gmail.com>
2023-03-15 20:14:31 +0100

    Version 1.6

--- src/api/true_or_false.py
+++ src/api/true_or_false.py
@@ -12,58 +12,51 @@ class TrueOrFalse:
         self.current_question: int = 0
         self.game_status = GameStatus.NOT_STARTED
 
-    def question_generation(self) -> str:
+    def generate_questions(self) -> None:
 
         with open(self.game_file, "r") as game_file:
             self.questions = {i: line.rstrip().split(";") for i, line in enumerate(game_file)}
 
+    def activate_the_game(self) -> None:
         self.game_status = GameStatus.IN_PROGRESS
-
-        return f"The questions have been generated"
 
     def question(self) -> str:
         user_question = self.questions[self.current_question]
         return user_question[0]
 
-    def path_to_file(self, file: str) -> str:
+    def set_the_file_path(self, file: str) -> None:
         self.game_file = Path(file)
-        return f"Path to file: {self.game_file}"
+        print(f"Path to file: {self.game_file}")
 
-    def set_the_number_of_attempts(self, number: int) -> str:
+    def set_the_number_of_attempts(self, number: int) -> None:
         self.number_of_attempts = number
-        return f"The number of attempts: {self.number_of_attempts}"
 
-    def next_question(self) -> str:
+    def next_question(self) -> None:
         self.current_question += 1
-
         try:
             self.questions[self.current_question]
         except KeyError:
             self.game_status = GameStatus.WON
-            return "You win!"
+        print(f"Current question: {self.current_question + 1}")
 
-        return f"Current question: {self.current_question + 1}"
+    def victory_check(self) -> None:
+        if self.number_of_attempts == 0:
+            self.game_status = GameStatus.LOST
+        try:
+            self.questions[self.current_question]
+        except KeyError:
+            self.game_status = GameStatus.WON
 
-    def give_answer(self, answer: str) -> str:
+    def check_the_answer(self, answer) -> None:
+        user_question = self.questions[self.current_question]
+        if answer == user_question[1]:
+            print(f"This is the correct answer. {user_question[2]}")
+        else:
+            self.number_of_attempts -= 1
+            print(f"That's the wrong answer. {user_question[2]}")
 
+    def check_game_status(self) -> None:
         if self.game_status != GameStatus.IN_PROGRESS:
             raise InvalidOperationException(
                 f"Inappropriate status of game: {self.game_status}"
             )
-
-        user_question = self.questions[self.current_question]
-        if answer == user_question[1]:
-            return f"This is the correct answer. {user_question[2]}"
-        else:
-            self.number_of_attempts -= 1
-
-            if self.number_of_attempts == 0:
-                self.game_status = GameStatus.LOST
-                print(f"That's the wrong answer. {user_question[2]}")
-                return "You lost!"
-
-            return f"That's the wrong answer. {user_question[2]}"
-
-    @property
-    def check_game_status(self):
-        return self.game_status
--- src/main.py
+++ src/main.py
@@ -4,12 +4,26 @@ from src.enums import GameStatus
 
 game = TrueOrFalse()
 
-print(game.question_generation())
-print(game.set_the_number_of_attempts(int(input("Set the number of attempts: "))))
-print(game.check_game_status)
+game.activate_the_game()
+game.generate_questions()
+game.set_the_number_of_attempts(int(input("Set the number of attempts: ")))
+print(f"The number of attempts: {game.number_of_attempts}")
 
 while game.game_status == GameStatus.IN_PROGRESS:
-
+    game.check_game_status()
     print(game.question())
-    print(game.give_answer(input("Enter your answer: ")))
-    game.next_question()
+
+    answer = input("Enter your answer: ")
+    game.check_the_answer(answer)
+
+    game.victory_check()
+
+    if game.game_status == GameStatus.WON:
+        print("You win!")
+    elif game.game_status == GameStatus.LOST:
+        print("You lost!")
+    elif game.game_status == GameStatus.IN_PROGRESS:
+        game.next_question()
+
+
+
